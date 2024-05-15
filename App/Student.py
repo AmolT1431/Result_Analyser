0b1cfcd3ad1805a29cc205be03b65e80cd6c0048
amol <amolt.com>
2024-05-15 11:29:45 +0530

    OOP GUI

--- App/Student.py
+++ App/Student.py
@@ -1,12 +1,8 @@
 class Student:
     def __init__(self):
         self.Name=""
-        self.Prn=2023
-        self.Result="Pass"
-        self.Subject_list=[["AT","Pass",30],["Cpp","Pass",40]]
-
-st=Student()
-print(st.Name)
-print(st.Prn)
-print(st.Result)
-print(st.Subject_list)
+        self.Prn=0
+        self.Result=""
+        self.Subject_list=[]
+        self.Total=0
+        
